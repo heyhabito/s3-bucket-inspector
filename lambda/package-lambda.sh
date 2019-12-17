22e6cdfef453118ceb3b5145088b20bf427554ff
syst0m <tomislav.tomasic@habito.com>
2019-12-17 10:38:39 +0000

    Use virtualenv on the pipeline

--- lambda/package-lambda.sh
+++ lambda/package-lambda.sh
@@ -5,6 +5,12 @@ cd $(dirname "$0")
 
 rm -f function.zip
 
+# Setup virtualenv
+virtualenv --python=python3.7 v-env
+source v-env/bin/activate
+pip install requests
+deactivate
+
 # Package our lambda code without extra file attributes
 zip -X -r function.zip *
 
