80302f6bd0a1274f7151d26dc8e6a070e6edecac
syst0m <tomislav.tomasic@habito.com>
2019-12-18 17:57:54 +0000

    Fix lambda logging

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -6,11 +6,11 @@ from run import get_whitelist, key_from_output, set_of_issues, TestRunner
 
 from slack import send_diff_message, send_full_message
 
+log = logging.getLogger(__name__)
 
 def initialise_logging():
-    logging.basicConfig(
-        level=logging.INFO, format="[%(levelname)s] %(name)s: %(message)s"
-    )
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.INFO)
 
 
 def config_handler(event, context):  # pylint: disable=unused-argument
@@ -24,16 +24,22 @@ def run_handler(event, context):  # pylint: disable=unused-argument
     initialise_logging()
     config_bucket_name = os.environ["CONFIG_BUCKET"]
     output_bucket_name = os.environ["OUTPUT_BUCKET"]
+    log.info("## Getting configs")
     configs = get_configs(config_bucket_name)
     test_runner = TestRunner(output_bucket_name, *configs.values())
+    log.info("## Output:")
+    log.info(output)
     output = test_runner.run_and_upload(
         accounts=[k.split(".")[0] for k in configs.keys()]  # 123.json -> 123
     )
+    log.info("## Getting whitelist")
     whitelist = get_whitelist(config_bucket_name)
+    log.info("whitelist")
     if os.environ.get("DIFF_ONLY"):
         new, fixed = test_runner.diff_previous_s3(output, whitelist)
         send_diff_message(new, fixed, output_bucket_name, key_from_output(output))
     else:
+        log.info("## Sending to slack")
         send_full_message(
             set_of_issues(output, whitelist),
             output_bucket_name,
